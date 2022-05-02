27a031eea7f230a4b644b712c76246d3ec591dd3
Derek Wan <d.wan@icloud.com>
2022-05-02 21:15:40 +0800

    more

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from enum import auto
 from beartype import beartype
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.responses import Response
+from starlette.status import HTTP_200_OK
+from starlette.status import HTTP_404_NOT_FOUND
 from strenum import StrEnum
 
 
@@ -17,8 +20,18 @@ def _() -> JSONResponse:
 
 @app.get("/blog/all/")
 @beartype
-def _() -> JSONResponse:
-    return JSONResponse({"message": "All blogs provided"})
+def _(*, page: int = 1, page_size: int | None = None) -> JSONResponse:
+    return JSONResponse({"message": f"All {page_size} blogs on page {page}"})
+
+
+@app.get("/blog/{id}/comments/{comment_id}")
+@beartype
+def _(
+    *, id: int, comment_id: int, valid: bool = True, username: str | None = None
+) -> JSONResponse:
+    return JSONResponse(
+        {"message": f"blog_id {id}, {comment_id=}, {valid=}, {username=}"}
+    )
 
 
 class BlogType(StrEnum):
@@ -33,7 +46,12 @@ def _(*, type: BlogType) -> JSONResponse:
     return JSONResponse({"message": f"Blog type {type}"})
 
 
-@app.get("/blog/{id}/")
+@app.get("/blog/{id}/", status_code=HTTP_200_OK)
 @beartype
-def _(*, id: int) -> JSONResponse:
-    return JSONResponse({"message": f"Blog with id {id}"})
+def _(*, response: Response, id: int) -> JSONResponse:
+    if id > 5:
+        response.status_code = HTTP_404_NOT_FOUND
+        return JSONResponse({"error": f"Blog {id} not found"})
+    else:
+        response.status_code = HTTP_200_OK
+        return JSONResponse({"message": f"Blog with id {id}"})
