803f83cecfd25f830657574fabe63c50c6b78251
Derek Wan <d.wan@icloud.com>
2022-05-02 20:18:04 +0800

    wip

--- app/main.py
+++ app/main.py
@@ -1,27 +1,39 @@
+from enum import auto
+
+from beartype import beartype
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from strenum import StrEnum
 
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = False
-
-
-@app.get("/")
-def read_root() -> JSONResponse:
+@app.get("/hello/")
+@beartype
+def _() -> JSONResponse:
     return JSONResponse({"Hello": "World"})
 
 
-@app.get("/items/{item_id}")
-def read_item(*, item_id: int, q: str | None = None) -> JSONResponse:
-    return JSONResponse({"item_id": item_id, "q": q})
+@app.get("/blog/all/")
+@beartype
+def _() -> JSONResponse:
+    return JSONResponse({"message": "All blogs provided"})
 
 
-@app.put("/items/{item_id}")
-def update_item(*, item_id: int, item: Item) -> JSONResponse:
-    return JSONResponse({"item_name": item.name, "item_id": item_id})
+class BlogType(StrEnum):
+    short = auto()
+    story = auto()
+    howto = auto()
+
+
+@app.get("/blog/type/{type}/")
+@beartype
+def _(*, type: BlogType) -> JSONResponse:
+    return JSONResponse({"message": f"Blog type {type}"})
+
+
+@app.get("/blog/{id}/")
+@beartype
+def _(*, id: int) -> JSONResponse:
+    return JSONResponse({"message": f"Blog with id {id}"})
