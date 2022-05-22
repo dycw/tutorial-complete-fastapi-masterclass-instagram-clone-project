41052d13f85395d498d26f85578cf3e12be42127
Derek Wan <d.wan@icloud.com>
2022-05-23 07:40:57 +0800

    Finish Custom Response

--- src/tests/routers/test_product.py
+++ src/tests/routers/test_product.py
@@ -1,8 +1,11 @@
+from re import DOTALL
+from re import search
+
 from fastapi.testclient import TestClient
 from hypothesis import given
 from starlette.status import HTTP_200_OK
+from starlette.status import HTTP_404_NOT_FOUND
 
-from app.routers.product import product
 from tests.strategies import clients
 
 
@@ -13,4 +16,18 @@ from tests.strategies import clients
 def test_get(client: TestClient) -> None:
     r = client.get("/product/all")
     assert r.status_code == HTTP_200_OK
-    assert r.json() == product
+    assert r.text == "watch camera phone"
+
+
+@given(client=clients())
+def test_get_detail(client: TestClient) -> None:
+    r = client.get("/product/1")
+    assert r.status_code == HTTP_200_OK
+    assert search("<html>.*</html>", r.text.strip("\n"), flags=DOTALL)
+
+
+@given(client=clients())
+def test_get_detail_non_existent(client: TestClient) -> None:
+    r = client.get("/product/3")
+    assert r.status_code == HTTP_404_NOT_FOUND
+    assert r.text == "Product not available"
